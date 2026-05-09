514de0c12de900a0d66b89577a883e8e43aa32ed
Nidhi Biswas <nidhibiswas111@gmail.com>
2026-05-09 19:43:32 +0600

    add the functionality of accNo,pinNo for home page

--- home.js
+++ home.js
@@ -1,17 +1,30 @@
+const validPin=1234;
+
 document.getElementById('add-money-btn').addEventListener('click',function(e){
     e.preventDefault()
     
+
     const bank=document.getElementById('bank').value
     const accountNumber=document.getElementById('account-number').value
     const amount=parseInt(document.getElementById('add-amount').value)
-    const pin=document.getElementById('add-pin').value
+    const pin=parseInt(document.getElementById('add-pin').value)
 
+
+    if(accountNumber.length<11){
+        alert('please provide valid account number')
+        return;
+    }
+
+    if(pin!==validPin){
+        alert('please provide valid pin number')
+        return;    
+    }
+    
     const availableBalance=parseInt(document.getElementById('available-balance').innerText)
 
     const totalNewAvailableBalance=amount+availableBalance
 
     document.getElementById('available-balance').innerText=totalNewAvailableBalance
-    
 
 
     
